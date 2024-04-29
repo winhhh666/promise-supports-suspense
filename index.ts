fe77a731dc82ecdb38d20a8ad5ed4d30d43580a9
satan_pig <1048218204@qq.com>
2024-04-29 18:56:18 +0800

    转换器主体

---
+++ index.ts
@@ -0,0 +1,26 @@
+function wrapPromise(promise: Promise<any>) {
+	//设置两个变量, 一个是表示当前状态的变量, 一个是结果变量
+    let status = 'pending'
+    let result : any;
+    //利用传入的promise改变自身状态
+    const suspender = promise.then((r) => {
+        status = 'success',
+        result = r
+    }, (e) => {
+        status = 'error',
+        result = e
+    })
+    return({
+        //将read包裹在对象中暴露出去
+        read() {
+            if(status === 'pending') throw suspender
+            else if(status === 'error') throw result
+            else if(status === 'success') return result
+        }
+    })
+}
+
+export default function fetchData(url: string) {
+	promise = axios.get(url).then(res => res.data)
+    return wrapPromise(promise)
+}
